6e950191dd4fa1f65637e36077dad4a9efcd1f8f
RUN <kirill@gmaill.com>
2023-08-19 20:51:37 +0300

    ok

--- site/DJANGO/mysite/app_lesson4/models.py
+++ site/DJANGO/mysite/app_lesson4/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Lesson4(models.Model):
+    title=models.CharField("Загаловок",max_length=128)
+    Description=models.TextField("Описание")
+    Price=models.DecimalField("Цена",max_digits=8,decimal_places=2)
+    auction=models.BooleanField("ТОрг",help_text="если готовы торговаться отметье")
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.Price:.2f})"
+
+
+    class Meta:
+        db_table='advertisements'
--- site/DJANGO/mysite/app_lesson4/views.py
+++ site/DJANGO/mysite/app_lesson4/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from .models import Lesson4
 #from django.http import HttpResponse
 
 def index(request):
+    advertisements=Advertisements.object.all()
+    context={"advertisements":advertisements}
     return render(request,"index.html")
 
 def top_sellers(request):
     return render(request, "top-sellers.html")
+
+def advertisement_post(request):
+    form=AdvertisementForm()
+    context={"form":form}
+    return render (request,'avertisement-post.html',context)
